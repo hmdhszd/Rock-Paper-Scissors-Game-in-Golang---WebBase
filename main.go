07f37722d0656ddbf33ad579ea1fd57e205b16cf
hmdhszd <hmdhszd@gmail.com>
2021-10-18 21:48:55 +0330

    added playRound function

--- main.go
+++ main.go
@@ -1,31 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 
-	//-------------------------------------------------------------
+	renderTemplate(w, "index.html")
+}
 
-	w.Header().Set("Content-Type", "text/html")
-
-	html := `<h1>Hello World inside html tag int the go program!</h1>`
-	fmt.Fprint(w, html)
-
-	//-------------------------------------------------------------
-
-	fmt.Fprint(w, "Hello World!")
-
-	//-------------------------------------------------------------
+func playRound(w http.ResponseWriter, r *http.Request) {
 
 }
 
 func main() {
+
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/play", playRound)
 
 	log.Println("Starting web server on port 8080   -->   http://127.0.0.1:8080")
+
 	http.ListenAndServe(":8080", nil)
+
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+
+	//Parse the html file
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//Execute the html file
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
